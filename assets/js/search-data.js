b3509e5c83d5a1d0d5e8459a1ac6bb216791a7e2
caitclimate <caitclimate@gmail.com>
2025-09-17 22:31:04 +0000

    Deploying to gh-pages from @ caitclimate/caitclimate.github.io@8f7f1fdf35fb2227ffd47875e4aac781fbc7232d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,35 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%61%69%74%6D%61%72%74%69%6E%65%7A@%6D%69%61%6D%69.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://instagram.com/https://www.instagram.com/caitrocks/", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://orcid.org/https://orcid.org/0000-0001-8000-2134", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=xjJFnPAAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
